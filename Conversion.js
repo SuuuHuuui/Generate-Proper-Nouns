9d379784ec10735833a7cbe53b1d2b919a1ebd8d
SuuuHuuui <hui.su@autodesk.com>
2022-05-06 22:01:49 +0800

    Version2.1

--- Conversion.js
+++ Conversion.js
@@ -2,7 +2,7 @@
 * 更新功能：
 1.xlxs数据标题行自行读入数组，不再使用手动对照增加
 2.所得数组用module导出，不需要变成json
-* 版本号2.0
+* 版本号2.1
 */
 const xlsx = require("node-xlsx")
 const fs = require("fs")
@@ -10,36 +10,40 @@ const fs = require("fs")
 // 读取xlsx
 const sheets = xlsx.parse("./Employee_data.xlsx")
 
-// 获取xlsx第一个标签栏的数据,注意是从0开始的
-const sheetData = sheets[0].data
-
-// 定义数据列表
-let testTitle =  [] //装标题行
-
-let testList = [] //装标题行+数据行
-
-testTitle = sheetData[0]//读入标题行
-
-// 循环拼装数据
-sheetData.forEach((item, index) => 
+function GetExcelData ()
 {
-    if (index == 0)
-        {
-           return
-        } 
-    //数据行和标题行做拼接
-    else 
-        {
-            for(i = 0 ;i < testTitle.length ; i++)
+    // 获取xlsx第一个标签栏的数据
+    const sheetData = sheets[0].data
+
+    // 定义数据列表
+    let testTitle =  [] //装标题行
+
+    let testList = [] //装标题行+数据行
+
+    testTitle = sheetData[0]//读入标题行
+    
+    // 循环拼装数据
+    sheetData.forEach((item, index) => 
+    {
+        var vot = {}
+        if (index == 0)
             {
-                testList.push({
-
-                    [testTitle[i]] : item[i]
-
-                })
+                return
+            } 
+                
+        else 
+            {
+                  for(var i = 0 ; i < testTitle.length ; i++)
+                  {
+                        vot[testTitle[i]] = item[i]
+                  }
+                    
+                  testList.push(vot)
             }
-           
-        }
-       
-})
-module.exports.testList = testList
+        
+    })
+    return testList
+    //console.log("Can you see me?")
+}
+
+module.exports.GetExcelData_Function = GetExcelData//此处也要改，输出的是个函数
